d37c87e0c78bdfc70491960bebd5d3a85d68c82d
Steffen Schultz <steffenschultz@mailbox.org>
2019-09-18 10:39:06 +0200

    Pagesource: Add style support

--- pagesource/pagesource.php
+++ pagesource/pagesource.php
@@ -11,6 +11,7 @@ class YellowPagesource {
     // Handle initialisation
     public function onLoad($yellow) {
         $this->yellow = $yellow;
+        $this->yellow->system->setDefault("pagesourceStyle", "pagesource");
     }
     
     // Handle page layout
@@ -33,7 +34,9 @@ class YellowPagesource {
     public function onParseContentShortcut($page, $name, $text, $type) {
         $output = null;
         if ($name=="pagesource" && ($type=="block" || $type=="inline")) {
-            $output = "<div class=\"".htmlspecialchars($name)."\">\n";
+            list($style) = $this->yellow->toolbox->getTextArgs($text);
+            if (empty($style)) $style = $this->yellow->system->get("pagesourceStyle");
+            $output = "<div class=\"".htmlspecialchars($style)."\">\n";
             $output .= "<form class=\"pagesource-form\" action=\"".$this->yellow->page->getLocation(false)."\" method=\"post\">\n";
             $output .= "<input type=\"hidden\" name=\"status\" value=\"source\" />\n";
             $output .= "<input type=\"submit\" value=\"".$this->yellow->text->getHtml("EditToolbarPre")."\" class=\"btn source-btn\" />\n";
