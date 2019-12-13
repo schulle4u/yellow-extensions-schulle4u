063ce78842054f287dce6334ff9bc10d4dedb238
markseu <mark2011@mayberg.se>
2019-12-13 13:48:11 +0100

    Updated private extension

--- private/private.php
+++ private/private.php
@@ -1,10 +1,10 @@
 <?php
 // Private extension, https://github.com/schulle4u/yellow-extensions-schulle4u/tree/master/private
-// Copyright (c) 2018 Steffen Schultz
+// Copyright (c) 2018-2019 Steffen Schultz
 // This file may be used and distributed under the terms of the public license.
 
 class YellowPrivate {
-    const VERSION = "0.8.4";
+    const VERSION = "0.8.5";
     const TYPE = "feature";
     public $yellow;         //access to API
     
@@ -21,16 +21,16 @@ class YellowPrivate {
     // Handle page layout
     public function onParsePageLayout($page, $name) {
         if ($this->yellow->page->get("status")=="private" && $this->yellow->getRequestHandler()=="core") {
-            if (trim($_REQUEST["password"])!==$this->yellow->page->get("password")) {
+            if ($this->yellow->page->isExisting("password") && $this->yellow->page->get("password")==trim($_REQUEST["password"])) {
+                $this->yellow->page->setHeader("Last-Modified", $this->yellow->toolbox->getHttpDateFormatted(time()));
+                $this->yellow->page->setHeader("Cache-Control", "no-cache, must-revalidate");
+            } else {
                 $pageError = "<form class=\"private-form\" action=\"".$this->yellow->page->getLocation(true)."\" method=\"post\">\n";
-                $pageError .= "<p class=\"private-password\"><label for=\"password\">".$this->yellow->text->getHtml("editLoginPassword")."</label><br /><input type=\"password\" class=\"form-control\" name=\"password\" id=\"password\" value=\"".htmlspecialchars($_REQUEST["password"])."\" /></p>\n";
+                $pageError .= "<p class=\"private-password\"><label for=\"password\">".$this->yellow->text->getHtml("editLoginPassword")."</label><br /><input type=\"password\" class=\"form-control\" name=\"password\" id=\"password\" /></p>\n";
                 $pageError .= "<input type=\"submit\" value=\"".$this->yellow->text->getHtml("EditLoginButton")."\" class=\"btn login-btn\" />\n";
                 $pageError .= "</form>\n";
                 $this->yellow->page->error("401", $pageError);
-            } else {
-                $this->yellow->page->set("status", "none");
             }
         }
     }
-
 }
