63a0b5d9d60921f6183261491169e367d8f4a394
markseu <mark2011@mayberg.se>
2020-03-22 23:01:17 +0100

    Splitted HTML and JavaScript

--- spoiler/spoiler.js
+++ spoiler/spoiler.js
@@ -1,10 +1,28 @@
-function toggle_hidden(id, button) {
-    var x = document.getElementById(id);
-    if (x.style.display === "none") {
-        x.style.display = "block";
-        document.getElementById(button).setAttribute("aria-expanded", "true");
-    } else {
-        x.style.display = "none";
-        document.getElementById(button).setAttribute("aria-expanded", "false");
+var initSpoilerFromDOM = function() {
+    
+    // Toggle spoiler between hidden and visible
+    function onToggleSpoiler(e) {
+        e.stopPropagation();
+        e.preventDefault();
+        var elementLink = e.target;
+        if (elementLink) {
+            var id = elementLink.getAttribute("data-spoiler-id");
+            var elementText = document.getElementById(id);
+            if (elementText.style.display === "none") {
+                elementText.style.display = "block";
+                elementLink.setAttribute("aria-expanded", "true");
+            } else {
+                elementText.style.display = "none";
+                elementLink.setAttribute("aria-expanded", "false");
+            }
+        }
     }
-}
+    
+    // Initialise spoilers and bind events
+    var elements = document.getElementsByClassName("spoiler-link");
+    for (var i=0, l=elements.length; i<l; i++) {
+        elements[i].onclick = onToggleSpoiler;
+    }
+};
+
+window.addEventListener("DOMContentLoaded", initSpoilerFromDOM, false);
