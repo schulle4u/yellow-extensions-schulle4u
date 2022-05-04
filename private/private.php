2fd65f29a106bd95fb4828a8b43bdda8b9cec736
Steffen Schultz <steffenschultz@mailbox.org>
2022-05-04 11:16:39 +0200

    Private: Updated page error

--- private/private.php
+++ private/private.php
@@ -2,7 +2,7 @@
 // Private extension, https://github.com/schulle4u/yellow-extensions-schulle4u/tree/master/private
 
 class YellowPrivate {
-    const VERSION = "0.8.9";
+    const VERSION = "0.8.10";
     public $yellow;         //access to API
     
     // Handle initialisation
@@ -22,12 +22,12 @@ class YellowPrivate {
                 $this->yellow->page->setHeader("Last-Modified", $this->yellow->toolbox->getHttpDateFormatted(time()));
                 $this->yellow->page->setHeader("Cache-Control", "no-cache, must-revalidate");
             } else {
-                $pageError = $this->yellow->language->getText("privatePageError")."<br/>\n";
-                $pageError .= "<form class=\"private-form\" action=\"".$this->yellow->page->getLocation(true)."\" method=\"post\">\n";
-                $pageError .= "<p class=\"private-password\"><label for=\"password\">".$this->yellow->language->getTextHtml("editLoginPassword")."</label><br /><input type=\"password\" class=\"form-control\" name=\"password\" id=\"password\" /></p>\n";
-                $pageError .= "<input type=\"submit\" value=\"".$this->yellow->language->getTextHtml("EditLoginButton")."\" class=\"btn login-btn\" />\n";
-                $pageError .= "</form>\n";
-                $this->yellow->page->error("420", $pageError);
+                $errorMessage = $this->yellow->language->getText("privatePageError")."<br/>\n";
+                $errorMessage .= "<form class=\"private-form\" action=\"".$this->yellow->page->getLocation(true)."\" method=\"post\">\n";
+                $errorMessage .= "<p class=\"private-password\"><label for=\"password\">".$this->yellow->language->getTextHtml("editLoginPassword")."</label><br /><input type=\"password\" class=\"form-control\" name=\"password\" id=\"password\" /></p>\n";
+                $errorMessage .= "<input type=\"submit\" value=\"".$this->yellow->language->getTextHtml("EditLoginButton")."\" class=\"btn login-btn\" />\n";
+                $errorMessage .= "</form>\n";
+                $this->yellow->page->error("420", $errorMessage);
             }
         }
     }
